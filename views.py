e7d0ac3bb6d1b0122be082b4bee4c8ddc155576b
Antonin F <antonin1236.f@gmail.com>
2019-12-05 19:05:36 +0100

    commit 3

--- views.py
+++ views.py
@@ -2,12 +2,18 @@ from flask import Flask
 
 app = Flask(__name__)
 
+
+
+app.config.from_object('config')
+
 @app.route('/')
-
-#app.config.from_object('config')
-
 def index():
     return "Hello world !"
+
+
+@app.route('/home')
+def home():
+	return 'home'
 
 if __name__ == "__main__":
     app.run()
